1abeaaa1485a70440095d1dd1a4f448ea939294b
k.talanda <kamil0916@gmail.com>
2016-05-04 14:18:52 +0200

    clean database branch

--- app/src/main/java/com/example/datapersistancesample/data/CommonStorage.java
+++ app/src/main/java/com/example/datapersistancesample/data/CommonStorage.java
@@ -1,15 +1,12 @@
 package com.example.datapersistancesample.data;
 
 import android.content.ContentValues;
-import android.content.SharedPreferences;
 
 import com.example.datapersistancesample.data.database.Category;
 import com.example.datapersistancesample.data.database.Product;
-import com.example.datapersistancesample.data.file.FileAccess;
 import com.squareup.sqlbrite.BriteDatabase;
 
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -17,31 +14,12 @@ import rx.Observable;
 
 public class CommonStorage {
 
-    @Inject
-    SharedPreferences preferences;
 
     @Inject
     BriteDatabase briteDatabase;
 
     @Inject
-    FileAccess fileAccess;
-
-    @Inject
     CommonStorage() {
-    }
-
-    public void storeString(String key, String value) {
-        preferences.edit()
-                .putString(key, value)
-                .apply();
-    }
-
-    public String getString(String key) {
-        return preferences.getString(key, "");
-    }
-
-    public boolean isAvailable(String pref) {
-        return preferences.contains(pref);
     }
 
     public Observable<List<Category>> getCategoryList() {
@@ -67,15 +45,4 @@ public class CommonStorage {
         briteDatabase.delete(Product.TABLE_NAME, Product.ID + " = ?", String.valueOf(productId));
     }
 
-    public Observable<List<Map<String, String>>> getNoteList() {
-        return fileAccess.getFileContentList();
-    }
-
-    public Observable<Map<String, String>> addNote(String content) {
-        return fileAccess.addFileWithStringContent(content);
-    }
-
-    public Observable<Boolean> removeNote(String name) {
-        return fileAccess.removeFile(name);
-    }
 }
